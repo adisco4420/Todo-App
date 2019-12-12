e255c5105a5cbc10d847a87ece5d9585e2001661
alabi sodiq <adisco4420@gmail.com>
2019-12-12 11:36:27 +0100

    CRUD User

--- src/user/List.jsx
+++ src/user/List.jsx
@@ -1,86 +1,151 @@
 import React, { Component } from 'react';
-import { Table, Divider, Icon, Modal } from 'antd';
-const columns = [
-    {
-        title: 'Id',
-        dataIndex: 'id',
-        key: 'id',
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name'
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <span>
-          <span><a href=""><Icon type="eye" /> </a></span>
-          <Divider type="vertical" />
-          <span onClick={() => console.log(record)} ><Icon type="edit" /></span>
-          <Divider type="vertical" />
-          <span><Icon type="delete" /></span>
-        </span>
-      ),
-    },
-  ];
-  
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      id: 32,
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      id: 42,
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      id: 32,
-    },
-  ];
-class ListUsers extends Component {
-  state = { visible: false };
+import { Table, Divider, Icon, Modal, Input, Spin } from 'antd';
+import axios from 'axios';
+const api = 'https://agile-refuge-28120.herokuapp.com/api';
 
+  
+class ListUsers extends Component {
+  state = { 
+    visible: false,
+    confirmLoading: false,
+    value: null,
+    actionType: 'Add',
+    users: null,
+    userId: '',
+    columns:  [
+      {
+          title: 'Id',
+          dataIndex: 'id',
+          key: 'id',
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: 'Created At',
+        dataIndex: 'created_at',
+        key: 'created_at'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <span><a href=""><Icon type="eye" /> </a></span>
+            <Divider type="vertical" />
+            <span onClick={() => this.handleUpdate(text)} ><Icon type="edit" /></span>
+            <Divider type="vertical" />
+            <span onClick={() => this.handleDelete(text)}><Icon type="delete" /></span>
+          </span>
+        ),
+      },
+    ]
+  };
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers = async () => {
+      try {
+        const res = await axios.get(api + '/user');
+        this.setState({users: res.data})
+      } catch (error) {
+        console.log(error.response);
+      }
+  }
+  addUser = async () => {
+    try {
+      const user = {name: this.state.value}
+      await axios.post(api + '/user', user);
+      this.setState({visible: false,  confirmLoading: false, value: ''})
+      Modal.success({content: 'User Added Successfully'});
+      this.fetchUsers();
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+  updateUser = async () => {
+    try {
+      await axios.put(api + `/user/${this.state.userId}`, {name: this.state.value});
+      this.setState({visible: false,  confirmLoading: false})
+      this.fetchUsers();
+      Modal.success({content: 'User Updated Successfully'});
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+  deleteUser = async () => {
+    try {
+      await axios.delete(api + `/user/${this.state.userId}`)
+      this.setState({visible: false,  confirmLoading: false, value: ''})
+      this.fetchUsers();
+      Modal.success({content: 'User Deleted Successfully'});
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+  handleDelete = (user) => {
+    this.setState({
+      visible: true, actionType: 'Delete', 
+      value: user.name, userId: user.id})
+  }
+  handleUpdate = (user) => {
+    this.setState({
+      actionType: 'Edit', value: user.name, 
+      userId: user.id, visible: true});
+  }
   showModal = () => {
     this.setState({
       visible: true,
+      actionType: 'Add'
     });
   };
+
 
   handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
+    this.setState({confirmLoading: true})
+    const { actionType } = this.state;
+    if(actionType === 'Edit') {
+      this.updateUser()
+    } else if(actionType === 'Delete') {
+      this.deleteUser();
+    } else {
+      this.addUser()
+    }
   };
 
-  handleCancel = e => {
-    this.setState({
-      visible: false,
-    });
-  };
+    handleCancel = e => {
+      this.setState({ visible: false, value: ''});
 
-    handleClick() {
-      console.log('click');
+    };
+    handleChange({ target }) {
+      this.setState({value: target.value});
     }
     render() { 
         return (
           <React.Fragment>
-            <Table columns={columns} dataSource={data} />
+            {
+              this.state.users ?
+            <Table columns={this.state.columns} dataSource={this.state.users} rowKey="id" />
+            :
+            <div style={{justifyContent: 'center', display: 'flex'}}>
+              <Spin size="large" />
+            </div>
+            }
             <Modal
-                title="Basic Modal"
+                title={this.state.actionType + ' User'}
                 visible={this.state.visible}
                 onOk={this.handleOk}
                 onCancel={this.handleCancel}
+                confirmLoading={this.state.confirmLoading}
               >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+          {this.state.actionType !== 'Delete' ? 
+          <Input placeholder="Full Name" onChange={value => this.handleChange(value)} value={this.state.value} />
+          : <p>Are You Sure You want to Delete <b>{this.state.value}</b> </p>
+          }
           </Modal>
         </React.Fragment>
         );
